d7bc6c1c05317707c0c4aae267e13ec5f6e00d44
Alexis Fabian Soto Santana <afsoto@jerarquicos.com>
2021-11-17 20:26:40 -0300

    formato

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-
-import './App.css';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
-    <div className="container">
-   <h1 className="title">
-      Culito roto <span aria-label="emoji" role="img">🔥</span>
-   </h1>
-   
-</div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+    <Navbar />
+    <About />
+    <Projects />
+    <Skills />
+    <Testimonials />
+    <Contact />
+  </main>
   );
 }
 
